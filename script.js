6d3a79ca00115e732ee3a3a5dda60f4c9e2c2472
AngelinaRajesh <>
2021-05-08 04:30:54 +0000

    let statement

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.getElementById('button').addEventListener('click', function () {
 
-  let; const a = Number(document.getElementById('inputa').value)
-  let; const b = Number(document.getElementById('inputb').value)
+  let
+  const a = Number(document.getElementById('inputa').value)
+  let
+  const b = Number(document.getElementById('inputb').value)
+  let
   const h = Number(document.getElementById('inputh').value)
 
   alert(((a + b) / 2) * h)
