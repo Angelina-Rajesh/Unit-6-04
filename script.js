3dea1684bcd93bd8743567ee7e590d086caa797a
AngelinaRajesh <>
2021-05-08 04:36:34 +0000

    variables

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 document.getElementById('button').addEventListener('click', function () {
 
-  let
-  a = Number(document.getElementById('inputa').value)
-  let
-  b = Number(document.getElementById('inputb').value)
-  let
-  h = Number(document.getElementById('inputh').value)
+  let const = Number(document.getElementById('inputa').value)
+  let const = Number(document.getElementById('inputb').value)
+  let const = Number(document.getElementById('inputh').value)
 
   alert(((a + b) / 2) * h)
 })
