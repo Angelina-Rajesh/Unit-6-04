7e9872ce0e07ff6399353dde921a2a619cd4ca4d
AngelinaRajesh <>
2021-05-08 04:21:05 +0000

    parsing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('button').addEventListener('click', function () {
 
-  let const a = Number(document.getElementById('inputa').value)
+  let; const a = Number(document.getElementById('inputa').value)
   let const b = Number(document.getElementById('inputb').value)
   let const h = Number(document.getElementById('inputh').value)
 
