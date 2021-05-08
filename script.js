54a8e43f6a7c5d425ad276c159b2cd9146bbed29
AngelinaRajesh <>
2021-05-08 04:23:32 +0000

    more parsing

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('button').addEventListener('click', function () {
 
   let; const a = Number(document.getElementById('inputa').value)
-  let const b = Number(document.getElementById('inputb').value)
-  let const h = Number(document.getElementById('inputh').value)
+  let; const b = Number(document.getElementById('inputb').value)
+  let; const h = Number(document.getElementById('inputh').value)
 
   alert(((a + b) / 2) * h)
 })
