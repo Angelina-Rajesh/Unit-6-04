9553c9cd08da11fc8cd50c6cc1967b6a49c3c5b0
AngelinaRajesh <>
2021-05-08 04:26:28 +0000

    let error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('button').addEventListener('click', function () {
 
   let; const a = Number(document.getElementById('inputa').value)
   let; const b = Number(document.getElementById('inputb').value)
-  let; const h = Number(document.getElementById('inputh').value)
+  const h = Number(document.getElementById('inputh').value)
 
   alert(((a + b) / 2) * h)
 })
