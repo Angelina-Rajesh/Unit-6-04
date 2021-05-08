14ff61f62390dca014aef17f2fbedf1a2367bd1e
AngelinaRajesh <>
2021-05-08 04:33:16 +0000

    constants

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('button').addEventListener('click', function () {
 
   let
-  const a = Number(document.getElementById('inputa').value)
+  a = Number(document.getElementById('inputa').value)
   let
-  const b = Number(document.getElementById('inputb').value)
+  b = Number(document.getElementById('inputb').value)
   let
-  const h = Number(document.getElementById('inputh').value)
+  h = Number(document.getElementById('inputh').value)
 
   alert(((a + b) / 2) * h)
 })
