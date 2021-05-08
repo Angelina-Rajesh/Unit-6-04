19c928f6161475f40f5bb7e465547aa45467b56e
AngelinaRajesh <>
2021-05-08 03:12:44 +0000

    fixed

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function addition() {
   a = Number(parseInt(document.getElementById('inputa').value))
   b = Number(parseInt(document.getElementById('inputb').value))
   h = Number(parseInt(document.getElementById('inputh').value))
-  areaOfTrapezoid = ((a+b)/2)*h
+  areaOfTrapezoid = ((a + b) / 2) * h
   alert('Area of trapezoid = ' + Number(areaOfTrapezoid))
 }
 
-document.getElementById('button').addEventListener ('click',addition)
+document.getElementById('button').addEventListener ('click', addition)
