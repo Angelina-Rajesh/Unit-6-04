d532355e4214a49ebf52e5838b45f051423943e2
AngelinaRajesh <>
2021-05-09 23:13:22 +0000

    padded lines

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-document.getElementById('button').addEventListener('click', function () {
+document.getElementById('button').addEventListener('click', function)
 
   const a = Number(document.getElementById('inputa').value)
   const b = Number(document.getElementById('inputb').value)
   const h = Number(document.getElementById('inputh').value)
 
   alert(((a + b) / 2) * h)
-})
